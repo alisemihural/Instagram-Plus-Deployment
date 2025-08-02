8b93ef2c1d8dedd8796b3581838f810e7a7b9e83
Ali Semih Ural <60106172+alisemihural@users.noreply.github.com>
2025-08-02 18:56:32 -0400

    Add AdBanner component and react-adsense dependency

---
+++ my-app/src/components/AdBanner.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AdSense from "react-adsense";
+
+const AdBanner = () => {
+  return (
+    <div className="ad-container" style={{ margin: "20px 0" }}>
+      <AdSense.Google
+        client="ca-pub-6848142315187904"
+        slot="8265609965"
+        style={{ display: 'block' }}
+        format="auto"
+        responsive="true"
+      />
+    </div>
+  );
+};
+
+export default AdBanner;
