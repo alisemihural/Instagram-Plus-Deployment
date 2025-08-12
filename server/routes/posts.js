28a4f7b5be5c6e619de4f2b5ad480cad04326244
Ali Semih Ural <60106172+alisemihural@users.noreply.github.com>
2025-08-12 07:06:43 -0400

    Add endpoint to get user posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import auth from '../middleware/auth.js'
-import { createPost, getFeed, likePost } from '../controllers/post.js'
+import { createPost, getFeed, likePost, getUserPosts } from '../controllers/post.js'
 
 const router = express.Router()
 
 router.get('/', getFeed)
+router.get('/user/:userId', getUserPosts)
 router.post('/', auth, createPost)
 router.patch('/:id/like', auth, likePost)
 
