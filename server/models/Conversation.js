038dfba28e5e5cda199123a2c4ef8d673a4ff75f
Asura369 <linkevin1220@gmail.com>
2025-08-17 00:53:26 -0400

    add message models

---
+++ server/models/Conversation.js
@@ -0,0 +1,11 @@
+// Path: ./server/models/Conversation.js
+import mongoose from 'mongoose'
+const { ObjectId } = mongoose.Schema.Types
+
+const conversationSchema = mongoose.Schema(
+    {
+        participants: [{ type: ObjectId, ref: 'User' }]
+    },
+    { timestamps: true })
+
+export default mongoose.model('Conversation', conversationSchema)
---
+++ server/models/Message.js
@@ -0,0 +1,14 @@
+// Path: ./server/models/Message.js
+import mongoose from 'mongoose'
+const { ObjectId } = mongoose.Schema.Types
+
+const messageSchema = mongoose.Schema(
+    {
+        conversation: { type: ObjectId, ref: 'Conversation', required: true },
+        sender: { type: ObjectId, ref: 'User', required: true },
+        text: { type: String, required: true },
+        seenBy: [{ type: ObjectId, ref: 'User' }]
+    },
+    { timestamps: true })
+
+export default mongoose.model('Message', messageSchema)
